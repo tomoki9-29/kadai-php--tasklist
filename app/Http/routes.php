027795072d9bc8389f47e639746c7e744a991542
amiyatomoki <tomoki9-29@ezweb.ne.jp>
2017-11-17 03:09:51 +0000

    user login

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -19,6 +19,11 @@ Route::get('/', function () {
 Route::get('signup', 'Auth\AuthController@getRegister')->name('signup.get');
 Route::post('signup', 'Auth\AuthController@postRegister')->name('signup.post');
 
+//ログイン機能
+Route::get('login', 'Auth\AuthController@getLogin')->name('login.get');
+Route::post('login', 'Auth\AuthController@postLogin')->name('login.post');
+Route::get('logout', 'Auth\AuthController@getLogout')->name('logout.get');
+
 /*//↑上記を下記のように書き換える
 Route::get('/','TasklistsController@index');*/
 
--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -17,8 +17,20 @@
                 <ul class="nav navbar-nav navbar-right">
                     <!--黒い帯の右上に新規タスクの投稿（リンク先create.blade.php）を追加--> 
                     <!--<li>{!! link_to_route('tasklists.create','新規タスクの投稿') !!}</li> -->
-                    <li>{!! link_to_route('signup.get', 'Signup') !!}</li>
-                    <li><a href="#">Login</a></li>
+                    @if (Auth::check())
+                        <li><a href="#">Users</a></li>
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">{{ Auth::user()->name }} <span class="caret"></span></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="#">My profile</a></li>
+                                <li role="separator" class="divider"></li>
+                                <li>{!! link_to_route('logout.get', 'Logout') !!}</li>
+                            </ul>
+                        </li>
+                    @else
+                        <li>{!! link_to_route('signup.get', 'Signup') !!}</li>
+                        <li>{!! link_to_route('login.get', 'Login') !!}</li>
+                    @endif
                 </ul>
             </div>
         </div>
